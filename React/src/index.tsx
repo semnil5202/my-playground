f812c8c8dc077a5519c4e7902ff675ef30201e27
semnil5202 <semnil5202@gmail.com>
2023-12-19 22:41:19 +0900

    chore: webpack react 초기 세팅 1.0.0

---
+++ React/src/index.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import './global.css';
+
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
+root.render(<App />);
---
+++ React/webpack.config.js
@@ -0,0 +1,66 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const production = (process.env.NODE_ENV = 'production');
+
+module.exports = {
+  mode: production ? 'production' : 'development',
+  devtool: production ? 'hidden-source-map' : 'eval',
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                noEmit: false,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|webp)$/,
+        type: 'asset',
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    port: 3333,
+    hot: true,
+    static: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
