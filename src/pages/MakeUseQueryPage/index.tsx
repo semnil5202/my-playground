520392d1689b54843c0826e97675d03521c69a1e
semnil5202 <semnil5202@gmail.com>
2024-03-13 19:29:04 +0900

    refactor: 기본 og title, description 없을 경우 동작 확인

--- src/pages/MakeUseQueryPage/index.tsx
+++ src/pages/MakeUseQueryPage/index.tsx
@@ -1,9 +1,11 @@
 import { Suspense } from 'react';
 import PostContainer from './components/PostContainer';
+import HelmetComp from '../ReactHelmet/components/HelmetComp';
 
 export default function MakeUseQueryPage() {
   return (
     <>
+      <HelmetComp title="쿼리" description="쿼리 페이지" />
       <div>React-Query의 useQuery, useMutation을 만들어 사용해보는 공간입니다.</div>
       <Suspense fallback={<div>loading...</div>}>
         <PostContainer />
--- src/pages/ReactHelmet/components/HelmetComp.tsx
+++ src/pages/ReactHelmet/components/HelmetComp.tsx
@@ -2,13 +2,15 @@ import { Helmet } from 'react-helmet-async';
 
 interface Props {
   title: string;
+  description: string;
 }
 
-const HelmetComp = ({ title }: Props) => {
+const HelmetComp = ({ title, description }: Props) => {
   return (
     <Helmet>
       <title>{title}</title>
       <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
     </Helmet>
   );
 };
